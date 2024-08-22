1b90f4be5332bbdd97758a6af83883c987fefd9e
Matthew <mvre282@yahoo.com>
2024-08-22 14:56:30 -0500

    final commit

---
+++ main.js
@@ -0,0 +1,31 @@
+function buttonPress(id, imgid) {
+  var currentid = localStorage.getItem("currentid");
+  var currentimgid = localStorage.getItem("currentimgid");
+  if (currentid) {
+    var currentPanel;
+    try {
+      currentPanel = document.getElementById(currentid);
+    } catch {}
+  }
+  const panel = document.getElementById(id);
+  if (window.getComputedStyle(panel).display == "none") {
+    panel.style.display = "block";
+    localStorage.setItem("currentid", id);
+    localStorage.setItem("currentimgid", imgid);
+  } else if (window.getComputedStyle(panel).display == "block") {
+    panel.style.display = "none";
+  }
+  if (currentid !== id) {
+    currentPanel.style.display = "none";
+    document.getElementById(currentimgid).src = "assets/images/icon-plus.svg";
+  }
+  swapImage(imgid);
+}
+function swapImage(id) {
+  const questionImage = document.getElementById(id);
+  if (questionImage.getAttribute("src") == "assets/images/icon-plus.svg") {
+    questionImage.src = "assets/images/icon-minus.svg";
+  } else {
+    questionImage.src = "assets/images/icon-plus.svg";
+  }
+}
